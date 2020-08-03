2616cc843a02b70631a2e62d92e3c24c894bf66f
Venky <venkateshkatta9705@gmail.com>
2020-08-03 20:52:22 +0530

    adding welcome route

--- app.py
+++ app.py
@@ -5,4 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/welcome")
+def welcome():
+    return "welcome to python flask"
+
+if __name__ == "__main__":
+    app.run()
+    
+    
  
