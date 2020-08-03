4a29bc383a376ad2c5a5b2b3bfc4dc4b0bedb502
Venky <venkateshkatta9705@gmail.com>
2020-08-03 14:51:55 +0530

    add one more route to the application

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app  = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/welcome')
+def welcome():
+    return "WELCOME TO FLASK"
 if __name__ == "__main__":
     
    app.run()
