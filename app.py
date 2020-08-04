6868138dc44d014e03a9a2bc6636107dff3e61b0
Venky <venkateshkatta9705@gmail.com>
2020-08-04 11:57:34 +0530

    weather-application

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask
+from flask import Flask,request,render_template
+import requests
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    return render_template("index.html")    
 
-@app.route("/welcome")
-def welcome():
-    return "welcome to python flask"
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/', methods=['POST'])
+def Onsubmit():
+    place = request.form['place']
+    # place_in_uppercase = place.upper()
+    API_KEY = 'e03b0e4011564a1a9e11eb7086d70d1e'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={place}&APPID={API_KEY}&units=metric'
+    weather_info = requests.get(url).json()
+    temparature = weather_info["main"]["temp"]
+    temp_in_string = str(temparature)
+    # return f'The current temparature in {place_in_uppercase} is {temp_in_string} \u00b0C'
+    return f'The current temparature in {place} is {temp_in_string} \u00b0C'
     
+if __name__ == "__main__":
+    app.run(debug=True)
+
     
  
