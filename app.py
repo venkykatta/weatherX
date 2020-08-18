94868191c87a6535f196255b9c973af5c32669b6
Venky <venkateshkatta9705@gmail.com>
2020-08-18 20:18:26 +0530

    add the bulmacard feature

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template , url_for , redirect    
 import requests
+
+from markupsafe import escape
 
 app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def index():
-    return render_template("index.html")    
+    return render_template("index.html") 
+
+@app.route('/users' , methods=['POST'])     
+def welcome():
+    return "welcome"   
 
 
 @app.route('/', methods=['POST'])
@@ -15,11 +21,15 @@ def Onsubmit():
     url = f'http://api.openweathermap.org/data/2.5/weather?q={place}&APPID={API_KEY}&units=metric'
     weather_info = requests.get(url).json()
     temparature = weather_info["main"]["temp"]
-    temp_in_string = str(temparature)
-    return f'The current temparature in {place} is {temp_in_string} \u00b0C'
+    temp_in_string = str(temparature)   
+    return redirect(url_for('Bulmacard')) 
+    # return f'The current temparature in {place} is {temp_in_string} \u00b0C'
     
-if __name__ == "__main__":
-    app.run(debug=True)
 
+@app.route('/card')
+def Bulmacard():
+    return render_template('bulmacard.html')
     
- 
+
+if __name__ == '__main__':
+    app.run(debug=True)
